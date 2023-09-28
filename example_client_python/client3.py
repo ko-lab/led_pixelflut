83e8b53d5e5659251979f66f0864636e43cbf967
Francis Duvivier <francisduvivier@users.noreply.github.com>
2023-09-28 04:55:35 +0200

    Autoformat client3.py

--- example_client_python/client3.py
+++ example_client_python/client3.py
@@ -8,14 +8,14 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
 def pixel(x, y, r, g, b, a=255):
-  try:
-    if a == 255:
-        sock.send(bytes('PX %d %d %02x%02x%02x\n' % (x, y, r, g, b), 'utf-8'))
-    else:
-        sock.send(bytes('PX %d %d %02x%02x%02x%02x\n' % (x, y, r, g, b, a), 'utf-8'))
-  except Exception as e:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
+    try:
+        if a == 255:
+            sock.send(bytes('PX %d %d %02x%02x%02x\n' % (x, y, r, g, b), 'utf-8'))
+        else:
+            sock.send(bytes('PX %d %d %02x%02x%02x%02x\n' % (x, y, r, g, b, a), 'utf-8'))
+    except Exception as e:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
 
 # A few helper functions:
 
@@ -26,7 +26,7 @@ def rect(x, y, w, h, r, g, b):
 
 rect(0, 0, 100, 100, 100, 100, 100)
 
-def worm(x=512, y=512, n=1000000, r=random.randint(0,255), g=random.randint(0,255), b=random.randint(0,255)):
+def worm(x=512, y=512, n=1000000, r=random.randint(0, 255), g=random.randint(0, 255), b=random.randint(0, 255)):
     while n:
         pixel(x, y, r, g, b, 25)
         x += random.randint(0, 2) - 1
@@ -35,12 +35,12 @@ def worm(x=512, y=512, n=1000000, r=random.randint(0,255), g=random.randint(0,25
 
 im = Image.open('test.png').convert('RGB')
 im.thumbnail((256, 256), Image.LANCZOS)
-x_offset=random.randint(0,1024-256)
-y_offset=random.randint(0,1024-256)
+x_offset = random.randint(0, 1024 - 256)
+y_offset = random.randint(0, 1024 - 256)
 _, _, w, h = im.getbbox()
 for x in range(w):
     for y in range(h):
         r, g, b = im.getpixel((x, y))
-        pixel(x+x_offset, y+y_offset, r, g, b)
+        pixel(x + x_offset, y + y_offset, r, g, b)
 
 worm()
