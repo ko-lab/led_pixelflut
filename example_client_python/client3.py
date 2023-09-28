505fea205a7c243c1443b7c15e68663753ab6a4f
Francis Duvivier <francisduvivier@gmail.com>
2023-09-28 19:02:06 +0200

    Client used during the event

--- example_client_python/client3.py
+++ example_client_python/client3.py
@@ -8,9 +8,11 @@ HOST = 'localhost'
 PORT = 10108
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
-
+DELAY_PER_PIXEL_S= 0.001
 
 def pixel(x, y, r, g, b, a=255):
+    # print('pixel')
+    # time.sleep(DELAY_PER_PIXEL_S)
     global sock
     if a == 255:
         sock.send(bytes('PX %d %d %02x%02x%02x\n' % (x, y, r, g, b), 'utf-8'))
@@ -28,34 +30,50 @@ def rect(x, y, w, h, r, g, b):
 
 rect(0, 0, 100, 100, 100, 100, 100)
 
+width = 1280
+height = 780
 
-def worm(x=512, y=512, n=1000000, r=255, g=255, b=255):
+
+def worm(x, y, n, r, g, b):
     global sock
+    while n:
+        pixel(x, y, r, g, b, 25)
+        x += random.randint(0, 2) - 1
+        y += random.randint(0, 2) - 1
+        n -= 1
+
+image_options = ['brucon_2023_logo.png', 'brucon_2023_logo.png', 'brucon_2023_logo.png', 'ko-lab.png', 'brucon_2023_logo.png', 'brucon_2023_logo.png', 'ko-lab.png',
+                 'brucon_2023_logo.png', 'brucon_2023_logo.png', 'brucon_2023_logo.png', 'ko-lab.png', 'brucon_2023_logo.png', 'brucon_2023_logo.png', 'ko-lab.png',
+                 'troll.png']
+print('starting')
+i=0
+while True:
+    print('starting loop '+str(i))
+    i=i+1
+    black = random.randint(0, 1) > 0
+    image_choice = random.randint(0, len(image_options));
     try:
-        while n:
-            pixel(x, y, r, g, b, 25)
-            x += random.randint(0, 2) - 1
-            y += random.randint(0, 2) - 1
-            n -= 1
+        im = Image.open(image_options[image_choice]).convert('RGB')
+        im.thumbnail((100, 100), Image.LANCZOS)
+        x_offset = random.randint(0, width - 256)
+        y_offset = random.randint(0, height - 256)
+        _, _, w, h = im.getbbox()
+        for x in range(w):
+            for y in range(h):
+                if black:
+                    r, g, b = (10, 10, 10)
+                else:
+                    r, g, b = im.getpixel((x, y))
+                if (r + g + b > 0) or black:
+                    pixel(x + x_offset, y + y_offset, r, g, b)
+
+        worm(random.randint(0, width), random.randint(0, height), 10000000, random.randint(0, 255),
+             random.randint(0, 255),
+             random.randint(0, 255))
     except Exception as e:
         print('Sock send error')
         print(e)
         sock.close()
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((HOST, PORT))
-
-
-while True:
-    im = Image.open('test.png').convert('RGB')
-    im.thumbnail((256, 256), Image.LANCZOS)
-    x_offset = random.randint(0, 1024 - 256)
-    y_offset = random.randint(0, 1024 - 256)
-    _, _, w, h = im.getbbox()
-    for x in range(w):
-        for y in range(h):
-            r, g, b = im.getpixel((x, y))
-            if r + g + b > 0:
-                pixel(x + x_offset, y + y_offset, r, g, b)
-
-    worm(random.randint(0, 1024), random.randint(0, 1024), 1000000, random.randint(0, 255), random.randint(0, 255),
-         random.randint(0, 255))
+    time.sleep(1)
